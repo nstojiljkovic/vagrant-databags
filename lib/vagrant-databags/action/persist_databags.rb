feef52a6c096f451f6cfbb3d8df0441baf11426d
Nikola Stojiljkovic <xection@gmail.com>
2018-07-30 13:55:44 +0200

    [BUGFIX] Respect cleanup_on_provision setting when performing recover
    Change-Id: I6188333f288187f576e3076d0d0867ab0bd6027c
    Related: #WC-33753

--- lib/vagrant-databags/action/persist_databags.rb
+++ lib/vagrant-databags/action/persist_databags.rb
@@ -28,14 +28,15 @@ module VagrantPlugins
         end
 
         def recover(env)
-          chef_provisioners = machine_chef_provisioners(env[:machine])
-          chef_provisioners.each do |chef|
-            provisioner_type = chef_provisioner_type(chef)
-            env[:ui].info "Cleaning up temporal data bags"
-            DataBagsContainer.instance.clean(env[:machine], provisioner_type)
+          if env[:machine].config.databags.cleanup_on_provision && env[:machine].config.databags.map.size > 0
+            env[:ui].detail "[vagrant-databags] Cleaning up temp data bags folder"
+            chef_provisioners = machine_chef_provisioners(env[:machine])
+            chef_provisioners.each do |chef|
+              provisioner_type = chef_provisioner_type(chef)
+              DataBagsContainer.instance.clean(env[:machine], provisioner_type)
+            end
           end
         end
-
       end
     end
   end
